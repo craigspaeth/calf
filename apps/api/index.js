46f44b16c2380e5151a268f047a2e9ad372c3105
Artsy <craigspaeth@gmail.com>
2016-04-07 00:47:00 -0400

    working through model abstraction

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,20 +1,26 @@
 import Koa from 'koa'
 import graphqlHTTP from 'koa-graphql'
-// import schema from './schema'
 import c from 'koa-convert'
-import { schema } from 'model'
+import { model, schema, $ } from 'model'
+// import schema from './schema'
 // import jwt from 'koa-jwt'
-
-const app = new Koa()
 // const { AUTH0_SECRET, AUTH0_ID } = process.env
-
 // app.use(c(jwt({
 //   secret: new Buffer(AUTH0_SECRET, 'base64'),
 //   audience: AUTH0_ID
 // })))
+
+const app = new Koa()
+
+model('Campaign', {
+  name: $.string().description('Name of campaign'),
+  startAt: $.string().description('Start at date'),
+  endAt: $.string().description('End at date')
+})
+
 app.use(async (ctx, next) => {
   return c(graphqlHTTP({
-    schema: schema,
+    schema: schema(),
     graphiql: true,
     rootValue: ctx.state
   }))(ctx, next)
