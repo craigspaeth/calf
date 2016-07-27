22a160ad23cc208d5a15634ed380ab3ede1b4b40
Craig Spaeth <craigspaeth@gmail.com>
2016-07-27 18:08:43 -0400

    cleanup

--- apps/api/schemas/index.js
+++ apps/api/schemas/index.js
@@ -1,3 +1,4 @@
+import { array, string } from 'joi'
 import joiql from 'joiql'
 import { campaign, campaigns, campaignMutation } from './campaign'
 import mongojs, { ObjectId } from 'promised-mongo'
@@ -9,7 +10,11 @@ const db = mongojs(MONGOHQ_URL)
 const api = joiql({
   query: {
     campaign: campaign,
-    campaigns: campaigns
+    campaigns: campaigns,
+    regions: array().items(string())
+      .description('Returns all regions of all campaigns.'),
+    channels: array().items(string())
+      .description('Returns all channels of all campaigns.')
   },
   mutation: {
     campaign: campaignMutation
@@ -30,6 +35,12 @@ api.on('query.campaigns', async ({ req, res }) => {
 api.on('mutation.campaign', async ({ req, res }) => {
   console.log('saving!', req.args)
   res.campaign = await db.campaigns.save(req.args)
+})
+api.on('query.regions', async ({ res }) => {
+  res.campaign = await db.campaigns.distinct('regions')
+})
+api.on('query.channels', async ({ res }) => {
+  res.campaign = await db.campaigns.distinct('channels')
 })
 
 export default api.schema
