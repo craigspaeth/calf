bb2a11a7869be8eaea81074b9dd04bbd64876c3f
Artsy <craigspaeth@gmail.com>
2016-08-02 12:03:05 -0400

    ctrler stuff

--- apps/campaigns/controller.js
+++ apps/campaigns/controller.js
@@ -1,10 +1,18 @@
 import api from 'api'
 import campaigns from './views'
+import Baobab from 'baobab'
+import { memoize } from 'lodash'
 
-const campaignAttrs = ['_id', 'name', 'startAt', 'endAt', 'channels', 'regions']
+const wrap = typeof window === 'undefined' ? (fn) => fn : memoize
+export const state = wrap(() => new Baobab({
+  campaigns: [],
+  campaign: null
+}))
 
 export const indexRoute = async (ctx, next) => {
-  const data = await api(`{ campaigns { ${campaignAttrs.join(' ')} } }`)
-  ctx.tree.set('campaigns', data.campaigns)
+  const data = await api(`{
+    campaigns { _id name startAt endAt channels regions }
+  }`)
+  state().set('campaigns', data.campaigns)
   ctx.render(campaigns)
 }
